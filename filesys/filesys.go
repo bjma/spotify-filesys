31baedfc70a0bfeb4ee5f8d2be565631613038f3
bjma <bjma@ucsc.edu>
2021-06-30 13:15:33 -0700

    dummy commit

--- filesys/filesys.go
+++ filesys/filesys.go
@@ -1,61 +1,14 @@
 package filesys
 
 import (
-    _"fmt"
+	_ "fmt"
 )
 
 var (
-    dir_tree  *Tree = nil
+	dir_tree *Tree = nil
 )
 
-// When things get bigger, we should actually build a clean filesystem
-// This system should act as the layout for our commands
-func ParseDir(T *Tree) []*Node {
-    var ret []*Node
-    return append(ret, &Node{
-        Name:     ".",
-        Format:   "root",
-        Children: T.Children,
-        Id:       "",
-
-        Num_children: len(T.Children),
-    })
-}
-
-// Finds the position of dirname relative to its directory level
-// If it's the last element, return -1
-// Else, return the max depth remaining relative to current level
-// Requirements: BFS
-func ScanDir(dir []*Node, dname string) ([]*Node, int) {
-    // We'd want to do this in a sort of BFS like fashion;
-    // Rather, find the level that `dname` belongs to,
-    // get the position of `dname` relative to that level (i.e. 5th child),
-    // and just return the remaining children left in the level
-    return nil, -1
-}
-
-// Runs DFS on given directory and returns the destination
-// NOTE: There's a problem where the node pointer returned 
-// is corrupted; that is, within the function scope of DFS,
-// the data exists, but once returned the object is empty.
-// This is most likely due to how we're dealing with pointers
-// (i.e. lack of understanding of Golang), but also might
-// be how we're recursing. 
-// Look into the recursive calls via *unit testing*,
-// maybe we need to approach this from a different angle.
-func DFS(dir []*Node, dirname string) *Node {
-    var ret *Node
-    
-    if dir == nil {
-        return nil
-    }
-
-    for _, subdir := range dir {
-        if subdir.Name == dirname {
-            ret = subdir
-            break
-        }
-        DFS(subdir.Children, dirname)
-    }
-    return ret // is returned nil most of the time
-}
+// Reads directory `dirname` and creates
+// an array of the total number of elements
+// 
+//func ScanDir(dirname string, dir []*Node) {}
--- filesys/tree.go
+++ filesys/tree.go
@@ -1,206 +1,207 @@
 package filesys
 
 import (
-    "bytes"
-    "fmt"
-    "github.com/zmb3/spotify"
+	"bytes"
+	"fmt"
+	"github.com/zmb3/spotify"
 
-    "github.com/bjma/spotify-filesys/api"
+	"github.com/bjma/spotify-filesys/api"
 )
 
 var (
-    flag = false
+	flag = false
 )
 
-// TODO:
-// Maybe clean up the actual design of the tree
-// i.e., design things with more intention
-// also, maybe use linked lists if possible, over arrays?
-// Use thsi as example:
-// https://github.com/rfbergeron/bompiler/blob/master/astree.c
-
-// Abstraction for tracks, artists, albums, playlists, and folders
 type Node struct {
-    Name         string
-    Format       string     // Tracks, Artists, Playlists, Folders
-    Children     []*Node    // If artist or folders, Children != nil to store playlists and folders
-    Id           spotify.ID // If album or playlist, Id != nil to retrieve tracks by ID
-    Num_children int
-    Is_leaf      bool // Makes it easier for us to print trees
+	Name         string
+	Format       string     // Tracks, Artists, Playlists, Folders
+	Children     []*Node    // If artist or folders, Children != nil to store playlists and folders
+	Id           spotify.ID // If album or playlist, Id != nil to retrieve tracks by ID
+	Num_children int
 }
 
-// Essentially the data structure that represents the filesystem;
-// Navigation and all other stuff will be done on a Tree (see `fs.go`)
-// NOTE: Filesystem should only be done with user library (can maybe try to add saved albums)
 type Tree struct {
-    Client       *spotify.Client // Reference to Spotify client for mounting purposes
-    Cwp          *Node           // Current working playlist (ls will display children of Cwp)
-    Children     []*Node         // Subdirectories
-    Num_children int
+	Client       *spotify.Client // Reference to Spotify client for mounting purposes
+	Cwp          *Node           // Current working playlist (ls will display children of Cwp)
+	Dir         []*Node          // Library tree
+	Num_children int
 }
 
 // Builds a directory tree
 func BuildTree(f map[string]string, opt string) *Tree {
-    nodes := parseLibrary(opt, f)
-    root := Node{
-        Name:         ".",
-        Format:       "root",
-        Children:     nodes,
-        Id:           "",
-        Num_children: len(nodes),
-    }
+    var libTree []*Node
 
-    return &Tree{
-        Cwp:          &root, // figure out what to do for root
-        Children:     nodes,
-        Num_children: len(nodes),
+    lib :=  parseLibrary(opt, f)
+	root := Node{
+		Name:         ".",
+		Format:       "root",
+		Children:     lib,
+		Id:           "",
+		Num_children: len(lib),
     }
+    libTree = append(libTree, &root)
+
+	return &Tree{
+		Cwp:          &root, // figure out what to do for root
+		Dir:     libTree,
+		Num_children: len(libTree),
+	}
 }
 
 // `tree` command
 func PrintTree(T *Tree, depth int) {
-    dir_tree = T
+	dir_tree = T
 
-    var w_buf bytes.Buffer
-    printTreeRecurse(T.Children, ".", depth, &w_buf, 0, false)
-    fmt.Println(w_buf.String())
+	var w_buf bytes.Buffer
+    //printTreeRecurse(T.Children, ".", depth, &w_buf, 0, false)
+    printTreeTest(T.Dir, &w_buf)
+	fmt.Println(w_buf.String())
 }
 
 // Parses entire user library for building filesystem
 func parseLibrary(opt string, folders map[string]string) []*Node {
-    var nodes []*Node
+	var nodes []*Node
 
-    switch opt {
-    case "user":
-        nodes = constructUserTree(folders)
-    case "artists":
-        nodes = constructArtistTree()
-    case "albums":
-        break
-    }
-    return nodes
+	switch opt {
+	case "user":
+		nodes = constructUserTree(folders)
+	case "artists":
+		nodes = constructArtistTree()
+	case "albums":
+		break
+	}
+	return nodes
+}
+
+// NOTE: Might just have a single function to construct trees
+// so that we don't clutter code
+func constructUserTree(folders map[string]string) []*Node {
+	var nodes []*Node
+
+	lib := api.FetchPlaylists()
+
+	i := 0
+	n := len(lib)
+
+	// Iterate through user's entire playlist library and initialize data needed to
+	// generate directory tree
+	for i < n {
+		uri := string(lib[i].URI)
+
+		if folders[uri] != "" {
+			node, iter := newFolder(lib, folders[uri], i, folders)
+			nodes = append(nodes, node)
+			i += iter
+		} else {
+			nodes = append(nodes, newPlaylist(lib[i].Name, lib[i].ID, i == n-1))
+			i++
+		}
+	}
+
+	return nodes
 }
 
 func constructArtistTree() []*Node {
-    var nodes []*Node
+	var nodes []*Node
 
-    lib := api.FetchArtists()
+	lib := api.FetchArtists()
 
-    i := 0
-    n := len(lib)
-    for i < n {
-        // Get list of artist albums
-        albums := api.FetchAlbums(lib[i].ID, "artist")
-        var children []*Node
+	i := 0
+	n := len(lib)
+	for i < n {
+		// Get list of artist albums
+		albums := api.FetchAlbums(lib[i].ID, "artist")
+		var children []*Node
 
-        for _, album := range albums {
-            children = append(children, &Node{
-                Name:         album.Name,
-                Format:       "album",
-                Children:     nil,
-                Id:           album.ID,
-                Num_children: 0,
-            })
-        }
+		for _, album := range albums {
+			children = append(children, &Node{
+				Name:         album.Name,
+				Format:       "album",
+				Children:     nil,
+				Id:           album.ID,
+				Num_children: 0,
+			})
+		}
 
-        nodes = append(nodes, &Node{
-            Name:         lib[i].Name,
-            Format:       "artist",
-            Children:     children,
-            Id:           "",
-            Num_children: len(children),
-            Is_leaf:      i == n-1,
-        })
-        i++
-    }
+		nodes = append(nodes, &Node{
+			Name:         lib[i].Name,
+			Format:       "artist",
+			Children:     children,
+			Id:           "",
+			Num_children: len(children),
+		})
+		i++
+	}
 
-    return nodes
-}
-
-func constructUserTree(folders map[string]string) []*Node {
-    var nodes []*Node
-
-    lib := api.FetchPlaylists()
-
-    i := 0
-    n := len(lib)
-    // Iterate through user's entire playlist library and initialize data needed to generate directory tree
-    for i < n {
-        uri := string(lib[i].URI)
-
-        if folders[uri] != "" {
-            node, iter := newFolder(lib, folders[uri], i, folders)
-            nodes = append(nodes, node)
-            i += iter
-        } else {
-            nodes = append(nodes, newPlaylist(lib[i].Name, lib[i].ID, i == n-1))
-            i++
-        }
-    }
-    // This is so stupid lol
-    nodes[len(nodes)-1].Is_leaf = true
-    return nodes
+	return nodes
 }
 
 // Initializes a folder Node by doing a depth-first search on a list of playlists
 // Returns offset for index to avoid unnecessary searches/parsing
 func newFolder(playlists []spotify.SimplePlaylist, dirname string, index int, folders map[string]string) (*Node, int) {
-    var children []*Node
-    iter := 0
+	var children []*Node
+	iter := 0
 
-    n := len(playlists)
-    for i := index; i < n && folders[string(playlists[i].URI)] == dirname; i++ {
-        playlist := playlists[i]
-        children = append(children, newPlaylist(playlist.Name, playlist.ID, false))
-        iter++
-    }
-    // Really jank way to guarantee that last directory entry is a leaf
-    children[len(children)-1].Is_leaf = true
+	n := len(playlists)
+	for i := index; i < n && folders[string(playlists[i].URI)] == dirname; i++ {
+		playlist := playlists[i]
+		children = append(children, newPlaylist(playlist.Name, playlist.ID, false))
+		iter++
+	}
 
-    node := &Node{
-        Name:         dirname,
-        Format:       "folder",
-        Children:     children,
-        Id:           "",
-        Num_children: len(children),
-        Is_leaf:      false,
-    }
-    return node, iter
+	node := &Node{
+		Name:         dirname,
+		Format:       "folder",
+		Children:     children,
+		Id:           "",
+		Num_children: len(children),
+	}
+	return node, iter
 }
 
 // Initializes a Node of format "Playlist"
 func newPlaylist(name string, playlist_id spotify.ID, is_leaf bool) *Node {
-    return &Node{
-        Name:         name,
-        Format:       "playlist",
-        Children:     nil,
-        Id:           playlist_id,
-        Num_children: 0,
-        Is_leaf:      is_leaf,
-    }
+	return &Node{
+		Name:         name,
+		Format:       "playlist",
+		Children:     nil,
+		Id:           playlist_id,
+		Num_children: 0,
+	}
 }
 
 // Helper function for treeRecurse that prints up to `depth` tracks
 // Also I want to get a better naming for this function
 func printTracks(id spotify.ID, format string, w_buf *bytes.Buffer, depth int, level int) {
-    var indent string
-    num_spaces := level * 3
-    for i := 0; i < num_spaces; i++ {
-        indent += " "
-    }
-    tracks := api.FetchTracks(id, depth, format)
-    for i, track := range tracks {
-        if flag {
-            w_buf.WriteString("|")
-        }
-        w_buf.WriteString(indent)
-        if i == len(tracks)-1 {
-            w_buf.WriteString("└──")
-        } else {
-            w_buf.WriteString("├──")
-        }
+	var indent string
+	num_spaces := level * 3
+	for i := 0; i < num_spaces; i++ {
+		indent += " "
+	}
+	tracks := api.FetchTracks(id, depth, format)
+	for i, track := range tracks {
+		if flag {
+			w_buf.WriteString("|")
+		}
+		w_buf.WriteString(indent)
+		if i == len(tracks)-1 {
+			w_buf.WriteString("└──")
+		} else {
+			w_buf.WriteString("├──")
+		}
 
-        w_buf.WriteString(track.Name)
-        w_buf.WriteString("\n")
+		w_buf.WriteString(track.Name)
+		w_buf.WriteString("\n")
+	}
+}
+
+func printTreeTest(tree []*Node, w_buf *bytes.Buffer) {
+    if tree == nil {
+        return
+    }
+
+    for _, node := range tree {
+        fmt.Printf("%s\n", node.Name)
+        printTreeTest(node.Children, w_buf)
     }
 }
